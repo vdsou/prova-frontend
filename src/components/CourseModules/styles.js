fe4a8f0752570b54d607aa3e96590387f6229107
vdsou <vdsouza@outlook.com>
2021-12-16 18:49:24 -0300

    fix layout issues

--- src/components/CourseModules/styles.js
+++ src/components/CourseModules/styles.js
@@ -26,41 +26,47 @@ export const LecturesQuantity = styled.span`
 `;
 
 export const Card = styled.button`
+  display: flex;
   overflow: hidden;
   justify-content: space-between;
-  min-height: 10rem;
+  min-height: 15rem;
+  background: tomato;
   min-width: clamp(80%, 20vw, 30rem);
   padding: 2rem;
   background: none;
   border: 0.1rem solid ${({ theme }) => theme.colors.primary};
   border-radius: 2rem;
-  display: flex;
   &:hover {
     transform: scale(1.01, 1.01);
     background: #282a36;
-  }
-  @media (max-width: 800px) {
-    flex-direction: column;
-    width: 93%;
-    height: 13.5rem;
-    align-items: center;
   }
   div {
     display: flex;
     flex-wrap: wrap;
     flex-direction: column;
     align-items: start;
+    width: 100%;
   }
   i {
     background: ${({ theme }) => theme.colors.primary};
-    height: 90%;
-    width: 20%;
+    height: 6rem;
+    width: 6rem;
     font-size: 2.4rem;
     display: flex;
     justify-content: center;
     align-items: center;
     border-radius: 1rem;
     color: ${({ theme }) => theme.colors.secondary};
+    @media (max-width: 800px) {
+      height: 5rem;
+      width: 5rem;
+    }
+  }
+  @media (max-width: 800px) {
+    flex-direction: column;
+    width: 93%;
+    min-height: 15rem;
+    align-items: center;
   }
 
   cursor: pointer;
